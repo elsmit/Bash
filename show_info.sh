6330857adb63ec1659bb679500c4bbf28c09bdf8
o.knyazev <o.knyazev@cdek.ru>
2019-12-23 13:02:59 +0700

    First script. Show some informaton about system

---
+++ show_info.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Сценарий который выводит дату, время, список пользователей, аптайм и сохраняет информацию в системном журнале
+
+#Дата
+date=$(date +%c)
+echo Дата: $date \
+
+#Время
+time=$(date +%H:%M:%S)
+echo Время: $time \
+
+#Список пользователей
+users=$(who)
+echo Пользователи: $users \
+
+#Аптайм
+uptime=$(uptime)
+echo Аптайм: $uptime  \
+
+#Сохраняем в системный журнал
+echo Дата: $date > /home/knyazev/system.log
+echo Время: $time >> /home/knyazev/system.log
+echo Пользователи: $users >> /home/knyazev/system.log
+echo Аптайм: $uptime >> /home/knyazev/system.log
+
